cec81c73ceb3d64bb79c921d6a61f8bec74add1b
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-08-29 10:10:40 +0600

    more bug solved

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../page/shered/Navbar";
 import Footer from "../page/shered/Footer";
-import ScrollToTop from "../componnets/scroltoTop/scrollToTop";
+import ScrollToTop from "../componnets/scroltoTop/ScrollsToTop";
 
 const Main = () => {
   return (
     <div className="overflow-hidden">
       <ScrollToTop />
       <Navbar />
-
       <Outlet />
-
       <Footer />
     </div>
   );
